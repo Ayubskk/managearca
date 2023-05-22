c4181dc8cf4d92ac8dbf1899d63c2318c7f4c6d6
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 06:56:28 +0700

    Update muting.py

--- YinsRobot/modules/muting.py
+++ YinsRobot/modules/muting.py
@@ -310,5 +310,5 @@ dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_BUTTON_HANDLER)
 
-__mod_name__ = "Muting"
+__mod_name__ = "ᴍᴜᴛɪɴɢ"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
