ceaeabf002cd7e1accb59db296d82437625d581c
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 06:54:22 +0700

    Update math.py

--- YinsRobot/modules/math.py
+++ YinsRobot/modules/math.py
@@ -97,7 +97,7 @@ def log(update: Update, context: CallbackContext):
     message.reply_text(math.log(int(args[0])))
 
 
-__mod_name__ = "Math"
+__mod_name__ = "ᴍᴀᴛʜ"
 
 SIMPLIFY_HANDLER = DisableAbleCommandHandler("math", simplify, run_async=True)
 FACTOR_HANDLER = DisableAbleCommandHandler("factor", factor, run_async=True)
