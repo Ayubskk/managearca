21a47bc814c5197be747c276d3d3b6e13038e86d
ayubskk <syallahudin75@gmail.com>
2023-05-22 20:29:50 +0700

    first commit

---
+++ YinsRobot/modules/alive.py
@@ -0,0 +1,25 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from YinsRobot.events import register
+from YinsRobot import telethn as tbot
+
+yinzver = "2.0.22"
+PHOTO = "https://telegra.ph/file/64f4b1e3ab6d6911447d2.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Yins Robot.** \n\n"
+  TEXT += "✨ **I'm Working Properly** \n\n"
+  TEXT += f"✨ **Oᴡɴᴇʀ : [AʏɪɪɴXᴅ](https://t.me/AyiinXd)** \n\n"
+  TEXT += f"✨ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ    :** `{telever}` \n\n"
+  TEXT += f"✨ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
+  TEXT += f"✨ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
+  TEXT += f"✨ **YɪɴᴢRᴏʙᴏᴛ Vᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ✨**"
+  BUTTON = [[Button.url("Help", "https://t.me/YinzRobot?start=help"), Button.url("Support", "https://t.me/AyiinXdSupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
