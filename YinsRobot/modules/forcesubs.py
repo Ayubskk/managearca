2827606d891682d2f83889fe74c0193668bfb3f8
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 06:45:30 +0700

    Update forcesubs.py

--- YinsRobot/modules/forcesubs.py
+++ YinsRobot/modules/forcesubs.py
@@ -200,4 +200,4 @@ Feds are now divided into 3 sections for your ease.
 • `/fedadminhelp`*:* Provides help for fed administration commands
 • `/feduserhelp`*:* Provides help for commands anyone can use
 """
-__mod_name__ = "F-Sub/Feds"
+__mod_name__ = "ꜰ-ꜱᴜʙꜱ"
