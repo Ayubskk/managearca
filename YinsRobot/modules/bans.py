dc856bf7176053cef2ea6d2ce34429dff12df02d
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 06:34:43 +0700

    Update bans.py

--- YinsRobot/modules/bans.py
+++ YinsRobot/modules/bans.py
@@ -592,7 +592,7 @@ __help__ = """
 """
 
 
-__mod_name__ = "Bans/Mutes"
+__mod_name__ = "ʙᴀɴ-ᴍᴜᴛᴇ"
 
 BAN_HANDLER = CommandHandler(["ban", "sban"], ban, run_async=True)
 TEMPBAN_HANDLER = CommandHandler(["tban"], temp_ban, run_async=True)
