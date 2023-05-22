43479747f2f6c4261aa08aed5c96d913be496663
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 06:34:01 +0700

    Update approve.py

--- YinsRobot/modules/approve.py
+++ YinsRobot/modules/approve.py
@@ -222,6 +222,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Approvals"
+__mod_name__ = "ᴀᴩᴩʀᴏᴠᴀʟ"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
