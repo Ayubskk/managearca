7efc8284f879f618253ae19f44359db39d34b513
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 08:30:13 +0700

    Update misc.py

--- YinsRobot/modules/helper_funcs/misc.py
+++ YinsRobot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Home", callback_data="yins_back")]]
+        pairs += [[EqInlineKeyboardButton("◁", callback_data="source_")]]
 
     return pairs
 
