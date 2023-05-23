d47f59a5edfebfad57506348fa8990d7a71c3903
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 09:31:06 +0700

    Update tagall.py

--- YinsRobot/modules/tagall.py
+++ YinsRobot/modules/tagall.py
@@ -117,6 +117,6 @@ Yins Can Be a Mention Bot for your group.
 
 Only admins can tag all.  here is a list of commands
 
-❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
+❂ /all or @all (reply to message or add another message) To mention all members in your group, without exception.
 ❂ /cancel for canceling the mention-all.
 """
