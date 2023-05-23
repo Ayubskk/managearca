c294290755ec1ab44af09614afc8492695476d33
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 08:38:51 +0700

    Update misc.py

--- YinsRobot/modules/helper_funcs/misc.py
+++ YinsRobot/modules/helper_funcs/misc.py
@@ -68,13 +68,49 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
-    elif calc == 2:
-        pairs.append((modules[-1],))
+
+    max_num_pages = ceil(len(pairs) / 4)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="fallen_back"),
+                EqInlineKeyboardButton(
+                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("◁", callback_data="source_")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back")]]
 
     return pairs
+
+
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
 
 
 def send_to_list(
@@ -125,6 +161,29 @@ def build_keyboard_parser(bot, chat_id, buttons):
             keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
         else:
             keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
+def user_bot_owner(func):
+    @wraps(func)
+    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == OWNER_ID:
+            return func(bot, update, *args, **kwargs)
+        else:
+            pass
+
+    return is_user_bot_owner
+
+
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
 
     return keyb
 
