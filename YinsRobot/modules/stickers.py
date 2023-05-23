f211282daf03aa4a71161c72781faf2248abde1a
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-05-23 07:04:12 +0700

    Update stickers.py

--- YinsRobot/modules/stickers.py
+++ YinsRobot/modules/stickers.py
@@ -574,7 +574,7 @@ def delsticker(update, context):
             "Please reply to sticker message to del sticker"
         )
 
-__mod_name__ = "Stickers"
+__mod_name__ = "ꜱᴛɪᴄᴋᴇʀ"
 
 __help__ = """
 *Help menu for stickers tools*
